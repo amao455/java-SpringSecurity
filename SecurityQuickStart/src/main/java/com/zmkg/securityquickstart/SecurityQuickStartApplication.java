b8d0bf854e2c275c5afaa5f4ef2ad0af78e77b8c
mqw <1315406616@qq.com>
2025-03-04 17:07:44 +0800

    3-4：初次接触SpringSecurity

---
+++ SecurityQuickStart/src/main/java/com/zmkg/securityquickstart/SecurityQuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.zmkg.securityquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityQuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityQuickStartApplication.class, args);
+    }
+
+}
---
+++ SecurityQuickStart/src/test/java/com/zmkg/securityquickstart/SecurityQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmkg.securityquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityQuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
